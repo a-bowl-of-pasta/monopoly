e5fdbbfb6d78ade86e21ad219edf82044221672c
SndMurph <51187728+SndMurph@users.noreply.github.com>
2025-04-05 19:46:22 -0500

    Add files via upload

---
+++ monopoly-main/src/driver_classes/main.java
@@ -0,0 +1,25 @@
+package driver_classes;
+
+import gui.*;  
+
+public class main {
+
+	
+	public static void testGameGUI()
+	{
+		new gui.gameGUI();
+		System.out.println("finished running constructor call");
+	}
+	
+	public static void testLoginGUI()
+	{
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		testGameGUI(); 
+	}
+	
+	
+}
